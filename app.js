df1ab78e082baa15076eae0c2e31072baf9b2f2b
agathanelle <agatageb@gmail.com>
2022-06-28 22:54:59 +0300

    Added declarations of mad-lib replacement variables

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var originalStory = '';
 //var userInputs = [];
+var numberOfQuestions = 10;
 
 originalStory = `In ${userInputs[0]} computer pioneer ${userInputs[1]} found herself working on ${userInputs[2]}. 
 Word got out that the team had "debugged" the ${userInputs[3]}. 
