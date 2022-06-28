ddc9c12dbc01f1aec9a6333d57fe21945705e7f9
agathanelle <agatageb@gmail.com>
2022-06-28 23:00:54 +0300

    For loop added

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ var originalStory = '';
 //var userInputs = [];
 var numberOfQuestions = 10;
 
+var userInputs = ['2090', 'Ashley', 'website', 'goverment system', 'John', 'working out', 'graduation party problem', 'Paul', 'whiskey', 'tea'];
+var questionArray = ['Enter year', 'Enter name', 'Enter noun', 'Enter noun', 'Enter name', 'Enter verb with -ing', 'Enter noun', 'Enter name', 'Enter noun', 'Enter noun'];
+
+for (var i = 0; i < questionArray.length; i++) {
+	console.log(questionArray[i]);
+}
+
 originalStory = `In ${userInputs[0]} computer pioneer ${userInputs[1]} found herself working on ${userInputs[2]}. 
 Word got out that the team had "debugged" the ${userInputs[3]}. 
 To her surprise ${userInputs[4]} has suggested ${userInputs[5]} to solve the ${userInputs[6]}. 
 In meanwhile ${userInputs[7]} started to drink ${userInputs[8]} and nearly split the ${userInputs[9]}`;
-
-var userInputs = ['2090', 'Ashley', 'website', 'goverment system', 'John', 'working out', 'graduation party problem', 'Paul', 'vodka', 'tea'];
