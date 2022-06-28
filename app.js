f9ea773bd3a1ce8355926d1fc44ee18dedc358b8
agathanelle <agatageb@gmail.com>
2022-06-28 23:17:19 +0300

    Polishing code

--- app.js
+++ app.js
@@ -12,9 +12,10 @@ for (var i = 0; i < questionArray.length; i++) {
 }
 alert('All done! Ready for your totally-accurate, not-at-all confusing history of technology??');
 
-var originalStory = `In ${userInputs[0]} computer pioneer ${userInputs[1]} found herself working on ${userInputs[2]}. 
-Word got out that the team had "debugged" the ${userInputs[3]}. 
-To her surprise ${userInputs[4]} has suggested ${userInputs[5]} to solve the ${userInputs[6]}. 
-In meanwhile ${userInputs[7]} started to drink ${userInputs[8]} and nearly split the ${userInputs[9]}`;
+var originalStory = `<h2>In ${userInputs[0]} computer pioneer ${userInputs[1]} found herself working on ${userInputs[2]}.</h2> 
+<h2>Word got out that the team had "debugged" the ${userInputs[3]}.</h2> 
+<h2>To her surprise ${userInputs[4]} has suggested ${userInputs[5]} to solve the ${userInputs[6]}.</h2> 
+<h2>In meanwhile ${userInputs[7]} started to drink ${userInputs[8]} and nearly spill the ${userInputs[9]}.</h2>`;
 
 console.log(originalStory);
+document.write(originalStory);
