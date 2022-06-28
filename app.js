769e1b45e9209aafff1c19979c44dcfbc0c5e88d
agathanelle <agatageb@gmail.com>
2022-06-28 22:53:58 +0300

    Added mad-lib replacement variables to story

---
+++ app.js
@@ -0,0 +1,9 @@
+var originalStory = '';
+//var userInputs = [];
+
+originalStory = `In ${userInputs[0]} computer pioneer ${userInputs[1]} found herself working on ${userInputs[2]}. 
+Word got out that the team had "debugged" the ${userInputs[3]}. 
+To her surprise ${userInputs[4]} has suggested ${userInputs[5]} to solve the ${userInputs[6]}. 
+In meanwhile ${userInputs[7]} started to drink ${userInputs[8]} and nearly split the ${userInputs[9]}`;
+
+var userInputs = ['2090', 'Ashley', 'website', 'goverment system', 'John', 'working out', 'graduation party problem', 'Paul', 'vodka', 'tea'];
